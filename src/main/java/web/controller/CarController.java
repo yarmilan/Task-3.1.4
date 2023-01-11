736848010a72fbba5382ec541ad9ba7813088004
yargo <yargomich@gmail.com>
2023-01-11 21:53:33 +0300

    2.2.2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
 import web.service.CarServiceImpl;
 
-import java.util.Optional;
+import java.util.List;
 
 @Controller
 public class CarController {
@@ -14,8 +15,10 @@ public class CarController {
     CarServiceImpl carService = new CarServiceImpl();
 
     @GetMapping(value = "/cars")
-    public String getCars(@RequestParam(name = "count", required = false) Optional<Integer> count, ModelMap model) {
-        model.addAttribute("cars", carService.getCars(count.orElse(0)).toString());
+    public String getCars(@RequestParam(name = "count", defaultValue = "5") int count, ModelMap model) {
+        List<Car> carList = carService.getCars(count);
+        model.addAttribute("list", carList);
         return "cars";
     }
+
 }
